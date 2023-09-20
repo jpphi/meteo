3bb5b2bd8e6b94700e1d8e1b9a08f3305a8c73ca
jpphi <jpphi@VMDeb.myguest.virtualbox.org>
2023-09-20 09:49:02 +0200

    JSON CURL GD + graphique de base OK sur carte et PC

--- README.md
+++ README.md
@@ -1,9 +1,35 @@
-# meteo
+# meteo  
   
-# Ressources  
+## Commande coté PC  
+Commande pour récupérer le fichier index.html depuis le PC:  
+wget 127.0.0.1:8080/index.html  
+  
+Compiler le programme pour PC, lcPC pour la carte lc.  
+  
+Le fichier index.html peut également être ouvert depuis mozilla à l'adresse:  
+http://127.0.0.1:8080/index.html  
+  
+## Commandes du coté de la carte  
+Dans le fichier /var/spool/cron/crontabs/jpm la ligne:  
+* * * * * /bin/ginfos  
+permet de lancer le programme pour les graphiques toutes les minutes.  
+Le programme peut être lancé manuellement par la commande **ginfos**.  
+  
+Pour lancer le daemon httpd:  
+httpd -u jpm  
+  
+Un fichier index.html a été crée et permet un affichage des graphiques produits depuis un navigateur.  
+  
+## Ressources  
 Prg de base interrogeant un site distant:  
 https://www.youtube.com/watch?v=QwCT7Ga4BG8  
   
-Ressources curl:  
+## Ressources curl:  
 https://curl.se/libcurl/c/libcurl-tutorial.html  
+
+### Ressource gdlib:  
+https://libgd.github.io/  
+  
+### Ressource json:
+https://json.nlohmann.me/  
 
--- ginfos.cpp
+++ ginfos.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 #include <curl/curl.h>
 #include <nlohmann/json.hpp>
-
+#include <gd.h>
+#include <vector>
 //#include <string.h>
 # include <fstream>
+#include<numeric>
 
 /*
 /home/jpphi/Downloads/buildroot-2023.08/output/build/libcurl-8.2.1/include/curl/curl.h
@@ -13,12 +15,22 @@
 using namespace std;
 using json= nlohmann::json_abi_v3_11_2::json;
 
+double maxTab(vector<double>);
+
 int main()
 {
+    // curl
     //char url[]= "api.openweathermap.org/data/2.5/weather?lat=43.5283&lon=5.44973&appid=fb7ea3ac85bb67a9bdb0b4b9a51f1c72&units=metric";
     char url[]= "api.open-meteo.com/v1/forecast?latitude=43.5283&longitude=5.4497&hourly=temperature_2m,windspeed_10m,winddirection_10m";
+
+    // json
     char nfichier[]= "rmeteo.json";
     FILE *desc;
+
+    // gd
+    long unsigned int COULEUR_FOND= 0x00FFFFFF;
+    gdImagePtr im;
+
 
     desc= fopen(nfichier,"w"); // On écrase à chaque fois le précédent relevé
 
@@ -56,18 +68,81 @@ int main()
     ifstream flux(nfichier);
 
     json j;
-    flux >> j;
+    flux >>j;
 
-    cout << j;
+    cout << j["hourly"]["temperature_2m"] ;
+    //j.value("hourly","temperature_2m");
+
+    vector<double> temperature2m(j["hourly"]["temperature_2m"]);
+
+    cout << "Taille: " << temperature2m.size();
+
+    double tempmax= maxTab(temperature2m);
+
+    cout << tempmax;
 
 
+    flux.close();
 
     //cout << valeur["latitude"];
+    int largeur= 800, hauteur= 800;
+    im = gdImageCreateTrueColor(largeur, hauteur);
+        gdImageFilledRectangle(im, 0, 0, largeur-1, hauteur-1, COULEUR_FOND);
 
+
+
+
+    for(int i=0; i<temperature2m.size()-1; i++)
+    {
+        double x0=i,y0= temperature2m[i],x1=i+1,y1= temperature2m[i+1];
+
+        //gdImageSetPixel(im,i,hauteur- temperature2m[i],0);
+        gdImageLine(im,x0,hauteur - y0,x1,hauteur - y1,0);
+        //gdImageFilledRectangle(im, x0, y0, x1, y0 - pourcentage[i], COULEURS[i]);
+
+        //printf("\nx0, y0= %d, %d - x1, y1 %d,%d - pourcentage[%d]= %d larg_h= %d espace== %d\n", x0,y0,x1,y0 - pourcentage[i],i, pourcentage[i], larg_h, espace);
+
+        //x0= x0 + larg_h + espace;
+        //x1= x1 + larg_h + espace;
+    }
+
+
+
+
+    // Sauvegarde du fichier
+    desc= fopen("temperatures.png","w"); // On écrase à chaque fois le précédent relevé
+
+    gdImagePng(im, desc);
+    fclose(desc);
+
+    gdImageDestroy(im);
 
     return 0;
 
 }
+
+
+
+double maxTab(vector<double> vt)
+{
+    double max= - numeric_limits<double>::max();
+
+    for(double v: vt)
+    {
+        if(v > max)max= v;
+    }
+    return max;
+}
+
+
+/*
+{
+"temperature_2m":[]
+"time":[]
+"winddirection_10m":[]
+"windspeed_10m":[]
+}
+*/
 
 /*
 
@@ -88,4 +163,31 @@ if (JSON_FOUND)
 endif()
 
 
+
+find_package(gd)
+
+if (gd_FOUND)
+    include_directories(${gd_INCLUDE_DIRS})
+    target_link_libraries(${PROJECT_NAME} ${gd_LIBRARIES})
+endif()
+
+
 */
+
+// INCLUDE
+/*
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <gd.h>
+#include <gdfontl.h>
+#include <gdfontt.h>
+#include <gdfonts.h>
+#include <gdfontmb.h>
+#include <gdfontg.h>
+#include <math.h>
+
+#define NBPOLICE 5
+*/
+
+//void afficheAide(void);
